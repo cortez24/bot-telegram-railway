435d5cf2cecc3954f4b0c1b5818ab5cf9c1550f8
cortez24 <ariyanto.tampubolon@gmail.com>
2025-06-15 19:45:15 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+def balas_pesan(update, context):
+    pesan = update.message.text
+    update.message.reply_text(f"Kamu mengirim: {pesan}")
+
+TOKEN = "8087748963:AAFaGZD6s4JwnzjcbVmjmK6ahUXMbnlfSVE"
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+handler = MessageHandler(Filters.text, balas_pesan)
+dispatcher.add_handler(handler)
+
+updater.start_polling()
+updater.idle()
