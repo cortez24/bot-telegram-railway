03c0f09e5b0e5ebabac8f57aeb8b9d0a7ed7beec
cortez24 <ariyanto.tampubolon@gmail.com>
2025-06-15 20:47:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-import os
 from telegram.ext import Updater, MessageHandler, Filters
 
 def balas_pesan(update, context):
     pesan = update.message.text
     update.message.reply_text(f"Kamu mengirim: {pesan}")
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = "8087748963:AAFaGZD6s4JwnzjcbVmjmK6ahUXMbnlfSVE"
+
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
@@ -14,3 +14,4 @@ dispatcher.add_handler(handler)
 
 updater.start_polling()
 updater.idle()
+
